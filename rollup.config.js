885888a5e548bb63838900f9d3ae6ae3a2544f52
Ian Richardson <iantrich@gmail.com>
2019-05-07 21:49:12 -0500

    Minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import babel from 'rollup-plugin-babel';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: ['src/card.ts'],
@@ -12,6 +13,7 @@ export default {
     resolve(),
     typescript(),
     babel({
-    exclude: 'node_modules/**'
-  })],
+      exclude: 'node_modules/**'
+    }),
+    terser()]
 };
