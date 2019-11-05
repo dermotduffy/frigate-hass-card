067e877de1be48afc861950d9fc7c3aedfc2d3f0
ludeeus <joasoe@gmail.com>
2019-11-05 21:31:25 +0100

    Don't serve during builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import babel from "rollup-plugin-babel";
-import serve from "rollup-plugin-serve";
 import { terser } from "rollup-plugin-terser";
 
 export default {
@@ -17,14 +16,5 @@ export default {
       exclude: "node_modules/**"
     }),
     terser(),
-    serve({
-      contentBase: "./dist",
-      host: "0.0.0.0",
-      port: 5000,
-      allowCrossOrigin: true,
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      }
-    })
   ]
 };
