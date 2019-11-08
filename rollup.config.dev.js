94af8728b9644910bdc71b2fd0a98b878c7307fa
Ian Richardson <iantrich@gmail.com>
2019-11-08 08:29:41 -0600

    remove babel and terser from dev rollup

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,8 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import babel from 'rollup-plugin-babel';
 import serve from 'rollup-plugin-serve';
-import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: ['src/boilerplate-card.ts'],
@@ -13,10 +11,6 @@ export default {
   plugins: [
     resolve(),
     typescript(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    terser(),
     serve({
       contentBase: './dist',
       host: '0.0.0.0',
