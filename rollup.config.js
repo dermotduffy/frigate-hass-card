859ab763407e558644b16f4356bfb9edd64e937f
ludeeus <joasoe@gmail.com>
2019-12-15 23:28:41 +0000

    Simplidy rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,42 @@
-import resolve from "rollup-plugin-node-resolve";
-import typescript from "rollup-plugin-typescript2";
-import babel from "rollup-plugin-babel";
-import { terser } from "rollup-plugin-terser";
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import serve from 'rollup-plugin-serve';
+import json from '@rollup/plugin-json';
 
-export default {
-  input: ["src/boilerplate-card.ts"],
-  output: {
-    dir: "./dist",
-    format: "es"
+const dev = process.env.ROLLUP_WATCH;
+
+const serveopts = {
+  contentBase: ['./dist'],
+  host: '0.0.0.0',
+  port: 5000,
+  allowCrossOrigin: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
   },
-  plugins: [
-    resolve(),
-    typescript(),
-    babel({
-      exclude: "node_modules/**"
-    }),
-    terser(),
-  ]
 };
+
+const plugins = [
+  nodeResolve({}),
+  commonjs(),
+  typescript(),
+  json(),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  dev && serve(serveopts),
+  !dev && terser(),
+];
+
+export default [
+  {
+    input: 'src/boilerplate-card.ts',
+    output: {
+      dir: 'dist',
+      format: 'es',
+    },
+    plugins: [...plugins],
+  },
+];
