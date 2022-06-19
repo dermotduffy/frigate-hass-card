8cfbe3958a610487d433b2797ffff4922384f247
Felipe Santos <felipecassiors@gmail.com>
2022-06-18 22:20:41 -0300

    Handle unpresence of .env better

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -7,7 +7,7 @@ readonly wanted_line="${wanted_line_key}='${PWD}'"
 readonly file=".env"
 
 echo "Writing ${wanted_line} to ${file}" >&2
-if grep --quiet "^${wanted_line_key}=" "${file}"; then
+if [[ -f "${file}" ]] && grep --quiet "^${wanted_line_key}=" "${file}"; then
     sed --in-place "s,^${wanted_line_key}=.*,${wanted_line}," "${file}"
 else
     echo "LOCAL_WORKSPACE_FOLDER='${PWD}'" >"${file}"
