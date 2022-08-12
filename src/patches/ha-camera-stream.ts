4be3797fe680df50d86f6568f4025a51d08bf323
Dermot Duffy <dermot.duffy@gmail.com>
2022-08-11 21:42:26 -0700

    Spacing fix.

--- src/patches/ha-camera-stream.ts
+++ src/patches/ha-camera-stream.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-nocheck
+// @ts-nocheck
 
 // ====================================================================
 // ** Keep modifications to this file to a minimum **
