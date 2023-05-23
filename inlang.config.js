508b96ebb2f0c0151e031973d131a4eed084eb0f
Niklas Buchfink <59048346+NiklasBuchfink@users.noreply.github.com>
2023-05-23 19:56:15 +0200

    Update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -1,16 +1,17 @@
 export async function defineConfig(env) {
 	const { default: pluginJson } = await env.$import(
-		'https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js'
+		"https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js"
 	);
 
 	const { default: standardLintRules } = await env.$import(
-		'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js'
+		"https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js"
 	);
 
 	return {
 		referenceLanguage: 'en',
 		plugins: [pluginJson({ 
-			pathPattern: './src/localize/languages/{language}.json'
+			pathPattern: "./src/localize/languages/{language}.json",
+			variableReferencePattern: ["{", "}"],
 		}), standardLintRules()]
 	};
 }
