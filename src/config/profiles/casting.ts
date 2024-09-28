8c5aebef0371cff4299a819a4bbcd36edfdaaa9f
Dermot Duffy <dermot.duffy@gmail.com>
2024-09-27 18:52:16 -0700

    fix: Don't hide menu in casting profile (#1590)

--- src/config/profiles/casting.ts
+++ src/config/profiles/casting.ts
@@ -5,11 +5,9 @@ import {
   CONF_LIVE_CONTROLS_BUILTIN,
   CONF_LIVE_SHOW_IMAGE_DURING_LOAD,
   CONF_MEDIA_VIEWER_CONTROLS_BUILTIN,
-  CONF_MENU_STYLE,
 } from '../../const.js';
 
 export const CASTING_PROFILE = {
-  [CONF_MENU_STYLE]: 'none' as const,
   [CONF_LIVE_AUTO_UNMUTE]: ['selected', 'visible'],
   [CONF_DIMENSIONS_ASPECT_RATIO]: '16:9',
   [CONF_LIVE_CONTROLS_BUILTIN]: false,
