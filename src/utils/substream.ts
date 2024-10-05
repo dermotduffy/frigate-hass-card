4185600da6fa879b8f361e13f98606ec6346715c
Dermot Duffy <dermot.duffy@gmail.com>
2024-10-05 14:14:19 -0700

    fix: Only show PTZ controls for selected camera in grid mode

--- src/utils/substream.ts
+++ src/utils/substream.ts
@@ -1,7 +1,11 @@
 import { View } from '../view/view';
 
 export const getStreamCameraID = (view: View, cameraID?: string): string => {
-  return view.context?.live?.overrides?.get(cameraID ?? view.camera) ?? view.camera;
+  return (
+    view.context?.live?.overrides?.get(cameraID ?? view.camera) ??
+    cameraID ??
+    view.camera
+  );
 };
 
 export const hasSubstream = (view: View): boolean => {
